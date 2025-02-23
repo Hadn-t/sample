9d6100141852927513203d874c4718f7d756cdbc
dasgajraj <dasgajrajg@gmail.com>
2025-02-23 16:22:49 +0530

    Reractor and correct GET api end points

--- land/next.config.ts
+++ land/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["192.168.1.7"], 
+  },
 };
 
 export default nextConfig;
